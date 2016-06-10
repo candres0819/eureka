a510f6015cdf687f9f9607f065effd39c1625b73
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-06-10 01:13:05 -0500

    config
    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- src/main/java/com/carloscardona/microservices/server/EurekaApplication.java
+++ src/main/java/com/carloscardona/microservices/server/EurekaApplication.java
@@ -1,12 +1,10 @@
 package com.carloscardona.microservices.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@EnableAutoConfiguration
 @SpringBootApplication
 public class EurekaApplication {
 
